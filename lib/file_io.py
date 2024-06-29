809ab66b155854b2917a5b6591041af668ea2729
Jonathan Marshall <jwmarsh16@gmail.com>
2024-06-29 01:39:57 -0500

    lab complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
-def write_file(file_name, file_content):
-    pass
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    """Write content to a .txt file"""
+    with open(f'{file_name}.txt', 'w') as file:
+        file.write(file_content)
+
+def append_file(file_name, file_content):
+    """Append content to a .txt file"""
+    with open(f'{file_name}.txt', 'a') as file:
+        file.write(file_content)
 
 def read_file(file_name):
+    """Read content from a .txt file"""
+    with open(f'{file_name}.txt', 'r') as file:
+        return file.read()
+
     pass
